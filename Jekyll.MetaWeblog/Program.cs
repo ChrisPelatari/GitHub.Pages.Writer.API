5200eec65bea548f394acb50c1823d8b9aa671e7
Chris Pelatari <chris@pelatari.com>
2022-10-13 22:00:01 -0500

    add metaweblog

--- Jekyll.MetaWeblog/Program.cs
+++ Jekyll.MetaWeblog/Program.cs
@@ -1,34 +1,16 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Jekyll.MetaWeblog;
+using WilderMinds.MetaWeblog;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddMetaWeblog<MetaWeblogProvider>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
+app.UseMetaWeblog("/metablog");
 
 app.Run();
-
-record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
