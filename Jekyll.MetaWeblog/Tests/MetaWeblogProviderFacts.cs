c72fd27657f4121ba0f7a711d87b7458d415bbb0
Chris Pelatari <chris@pelatari.com>
2022-11-15 01:01:32 -0600

    Update MetaWeblogProviderFacts.cs

--- Jekyll.MetaWeblog/Tests/MetaWeblogProviderFacts.cs
+++ Jekyll.MetaWeblog/Tests/MetaWeblogProviderFacts.cs
@@ -8,23 +8,24 @@ namespace Jekyll.MetaWeblog.Tests
 {
     public class MetaWeblogProviderFacts : IClassFixture<TestSetup>
     {
-        private readonly TestSetup _testSetup;
-        ServiceProvider _serviceProvider;
-        IConfiguration Config;
+        readonly TestSetup _testSetup;
+        readonly ServiceProvider _serviceProvider;
+        readonly IConfiguration? Config;
+        readonly MetaWeblogProvider metaWeblog;
 
         public MetaWeblogProviderFacts(TestSetup testSetup)
         {
             _testSetup = testSetup;
             _serviceProvider = testSetup.ServiceProvider;
             Config = _serviceProvider.GetService<IConfiguration>();
+            metaWeblog = new(Config);
         }
 
         [Fact]
         public async Task GetUserInfo_Should_return_user_info()
         {
             //arrange
-            var mwp = new MetaWeblogProvider(Config);
-            var userinfo = await mwp.GetUserInfoAsync("1", "ChrisPelatari", "3");
+            var userinfo = await metaWeblog.GetUserInfoAsync("1", "ChrisPelatari", "3");
             
 
             userinfo.Should().NotBeNull();
