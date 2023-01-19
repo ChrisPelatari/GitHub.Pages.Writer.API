75a555274e44737cc71ac5ffda448b5e01f3e501
Chris Pelatari <chris@pelatari.com>
2023-01-19 02:24:07 -0600

    Create EditPost.cs

---
+++ src/Tests/EditPost.cs
@@ -0,0 +1,33 @@
+namespace GitHub.Pages.Writer.API.Tests;
+
+public class EditPost : MetaWeblogProviderFacts
+{
+    public EditPost(WebApplicationFactory<Program> factory) : base(factory)
+    {
+    }
+
+    [Fact]
+    public async Task should_edit_post()
+    {
+        var post = new Post
+        {
+            title = "Test Post",
+            description = "Test Description",
+            dateCreated = DateTime.Now,
+            categories = new string[] { "Test Category" }
+        };
+
+        var result = await metaWeblog.AddPostAsync("1", "ChrisPelatari", "", post, true);
+
+        result.Should().Be($"{Config["blog:url"]}/{string.Join("/", post.categories)}/{post.dateCreated.Year}/{post.dateCreated.Month}/{post.dateCreated.Day}/{post.title}.html");
+
+        post.title = "Test Post 2";
+        post.description = "Test Description 2";
+        post.dateCreated = DateTime.Now;
+        post.categories = new string[] { "Test Category 2" };
+
+        var editResult = await metaWeblog.EditPostAsync("1", "ChrisPelatari", "", post, true);
+
+        editResult.Should().BeTrue();
+    }
+}
