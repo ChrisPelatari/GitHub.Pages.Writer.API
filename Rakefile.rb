2776e8d126176274f70be2dda6dace5e90a1e769
Chris Pelatari <chris@pelatari.com>
2023-01-21 12:36:25 -0600

    Update Rakefile.rb

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,4 +1,8 @@
-task :b dotnet:build do
+task :t do
+  sh "dotnet test src/Tests/Github.Pages.Writer.Tests.csproj"
+end
+
+task :b do
   sh "dotnet build src/API/Github.Pages.Writer.API.csproj"
 end
 
@@ -6,4 +10,4 @@ task :s do
   sh "dotnet run -p src/API/Github.Pages.Writer.API.csproj"
 end
 
-task :default => :s
+task :default => :b
